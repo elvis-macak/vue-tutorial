ebea90f90fdd45183d5ea4e36e327ff3c9782d49
elvis <elvis@lnmpy.com>
2017-06-20 12:45:38 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter'
+
+export default {
+  name: 'app',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="count">
+    <h1>{{ count }}</h1>
+    <button @click='increase()'>Increase</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'count',
+  data () {
+    return {
+      count: 1
+    }
+  },
+  methods: {
+    increase() {
+      this.count += 1;
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+</style>
